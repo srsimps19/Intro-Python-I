31b9c993b1de75859914cef640a4542ef7609cfd
Sydney Crumley <srsimps19@gmail.com>
2020-01-13 16:37:23 -0600

    finished 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,16 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+if len(sys.argv) == 1:
+  now = datetime.now()
+  print(calendar.monthcalendar(now.year, now.month))
+elif len(sys.argv) == 2:
+  now = datetime.now()
+  print(calendar).monthcalendar(now.year, int(sys.argv[1]))
+elif len(sys.argv) == 3:
+  now = datetime.now()
+  print(calendar).monthcalendar(int(sys.argv[2]), int(sys.argv[1]))
+else:
+  print('Invalid arguments please use this format: 14_cal.py month [year]')
+  exit()
